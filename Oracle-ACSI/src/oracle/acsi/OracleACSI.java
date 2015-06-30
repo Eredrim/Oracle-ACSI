d16264d110d1114b1214354e814ef833b6c6a5be
Gilles <Gilles@Gilles-PC>
2015-06-30 16:28:14 +0200

    Commit fin séance 30/06

--- Oracle-ACSI/src/oracle/acsi/OracleACSI.java
+++ Oracle-ACSI/src/oracle/acsi/OracleACSI.java
@@ -15,7 +15,9 @@ public class OracleACSI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MainUI mui = new MainUI();
+        mui.setVisible(true);
+        
     }
     
 }
